c9c7ba22791ee7ffa1d898fcfeef2f1710f5e3e1
BrandonMHerrin <brandon.herrin@live.com>
2025-04-05 00:09:21 -0600

    Add Swagger documentation and API routes
    - Implemented Swagger UI for API documentation in routes/swagger.js
    - Created swagger.js to generate Swagger documentation using swagger-autogen
    - Added initial swagger.json file with API definitions for Users, Products, Carts, and Orders
    - Developed utility function to load JSON files from the filesystem

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,109 @@ const userRoute = require('./users');
 const productRoute = require('./products');
 const cartRoute = require('./cart');
 const orderRoute = require('./orders');
+const swaggerRoute = require('./swagger');
+
+//Swagger routes, needed for swagger ui
+router.use('/', swaggerRoute);
 
 //All /users routes
-router.use('/users', userRoute);
+router.use(
+  "/users",
+  userRoute
+  /*
+        #swagger.tags = ['Users']
+        #swagger.responses[400] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/Errors'}
+        }
+        #swagger.responses[401] = {
+            ifStatusPresent: true,
+            description: 'Unauthorized. Authentication required.'
+        }
+        #swagger.responses[404] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/NotFound'}
+        }
+        #swagger.responses[500] = {
+            description: 'Server Error',
+            schema: {$ref: '#/definitions/Error'}
+        }
+    */
+);
 
 //All /product routes
-router.use('/product', productRoute);
+router.use(
+  "/product",
+  productRoute
+  /*
+        #swagger.tags = ['Product']
+        #swagger.responses[400] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/Errors'}
+        }
+        #swagger.responses[401] = {
+            ifStatusPresent: true,
+            description: 'Unauthorized. Authentication required.'
+        }
+        #swagger.responses[404] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/NotFound'}
+        }
+        #swagger.responses[500] = {
+            description: 'Server Error',
+            schema: {$ref: '#/definitions/Error'}
+        }
+    */
+);
 
 //All /cart routes
-router.use('/cart', cartRoute);
+router.use(
+  "/cart",
+  cartRoute
+  /*
+        #swagger.tags = ['Cart']
+        #swagger.responses[400] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/Errors'}
+        }
+        #swagger.responses[401] = {
+            ifStatusPresent: true,
+            description: 'Unauthorized. Authentication required.'
+        }
+        #swagger.responses[404] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/NotFound'}
+        }
+        #swagger.responses[500] = {
+            description: 'Server Error',
+            schema: {$ref: '#/definitions/Error'}
+        }
+    */
+);
 
 //All /order routes
-router.use('/order', orderRoute);
+router.use(
+  "/order",
+  orderRoute
+  /*
+        #swagger.tags = ['Order']
+        #swagger.responses[400] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/Errors'}
+        }
+        #swagger.responses[401] = {
+            ifStatusPresent: true,
+            description: 'Unauthorized. Authentication required.'
+        }
+        #swagger.responses[404] = {
+            ifStatusPresent: true,
+            schema: {$ref: '#/definitions/NotFound'}
+        }
+        #swagger.responses[500] = {
+            description: 'Server Error',
+            schema: {$ref: '#/definitions/Error'}
+        }
+    */
+);
 
 module.exports = router;
---
+++ routes/swagger.js
@@ -0,0 +1,12 @@
+const Router = require('express').Router;
+const swaggerUi = require('swagger-ui-express');
+const loadJson = require('../utils/load-json');
+
+const swaggerDoc = loadJson('../swagger/swagger.json');
+
+const router = Router();
+
+router.use('/api-docs', swaggerUi.serve);
+router.use('/api-docs', swaggerUi.setup(swaggerDoc));
+
+module.exports = router;
