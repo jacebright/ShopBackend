9f2b603c8f6edaee7e9369183ab2cb60a64d718d
desivar <jillian_desire@yahoo.es>
2025-04-05 00:09:14 -0600

    add configuration/connection.js add npm install sequelize pg pg-hstore

---
+++ configuration/connection.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Adjust based on your Render SSL settings
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      port: process.env.DB_PORT || 5432, // Default PostgreSQL port
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+      logging: false,
+    }
+  );
+}
+
+module.exports = sequelize;
