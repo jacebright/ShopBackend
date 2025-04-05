9eb1764e7afb51cfc4374e4a5b678ca7f113a0a4
jacebright <jacebright@gmail.com>
2025-04-05 14:25:57 -0500

    Corrected directory in swagger.js to prep for build

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,7 @@ const Router = require('express').Router;
 const swaggerUi = require('swagger-ui-express');
 const loadJson = require('../utils/load-json');
 
-const swaggerDoc = loadJson('../swagger/swagger.json');
+const swaggerDoc = loadJson('./swagger/swagger.json');
 
 const router = Router();
 
